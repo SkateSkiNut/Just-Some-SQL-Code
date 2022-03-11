99eb005adcb7aaa41192c4f446b1a77718176f79
SkateSkiNut <77465023+SkateSkiNut@users.noreply.github.com>
2022-03-11 13:25:02 -0600

    Create firstcode.sql

---
+++ firstcode.sql
@@ -0,0 +1,7 @@
+CREATE TABLE shopping_list(id INTEGER PRIMARY KEY AUTOINCREMENT, item_name TEXT, quantity INTEGER);
+
+INSERT INTO shopping_list(item_name, quantity) VALUES ("Screws, 200);
+INSERT INTO shopping_list(item_name, quantity) VALUES ("Bolts", 50);
+INSERT INTO shoppint_list(item_name, quantity) VALUES ("Nuts), 50);
+
+SELECT * FROM shopping_list;
